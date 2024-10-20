be0fa893abd67180937d846d403e33b1126486e4
Inna <prizrak1609@gmail.com>
2024-10-20 19:57:11 +0300

    Lesson3.1.1

--- Lesson3.1.1.cpp
+++ Lesson3.1.1.cpp
@@ -1,10 +1,40 @@
-/*Користувач вводить число. Визначити, чи є воно парним.*/
+/*1) Користувач вводить число. Визначити, чи є воно парним.
+2) Користувач вводить два числа. Вивести на екран менше з цих чисел.
+3) Користувач вводить число. Визначити, додатне це число, від'ємне чи дорівнює нулю.
+4) Користувач вводить два числа. Визначити, чи рівні ці числа, і якщо ні, вивести їх на екран за зростанням.*/
 
 #include<iostream>
 using namespace std;
 int main() {
-	int N;
-	cout << "Vvedite chuslo: ";
-	cin >> N;
-	cout << "\nVashe chuslo " << N <<  ((N % 2 == 0) ? " parne " : " ne parne\n");
+	int N1, N2, a;
+	cout << "Oberite zadachy:\n"
+		<< "1. Vuznachutu ch e vvedene chuslo parnum\n"
+		<< "2. Vuvestu menshe chuslo z dvoh chusel\n"
+		<< "3. Vuznachutu dane chuslo - dodatne, videmne, dorivnye nyly\n"
+		<< "4. Vuznachutu chu rivni dva chusla ta vuvestu ix za zrostannam\n";
+	cin >> a;
+	switch (a) {
+	case 1:
+		cout << "\nVvedite chuslo: ";
+		cin >> N1;
+		cout << "\nVashe chuslo " << N1 << ((N1 % 2 == 0) ? " parne\n" : " ne parne\n");
+		break;
+	case 2:
+		cout << "\nVvedite dva chusla: ";
+		cin >> N1 >> N2;
+		cout << "\nMenshe chuslo " << ((N1 < N2) ? N1 : N2) << endl;
+		break;
+	case 3:
+		cout << "\nVvedite chuslo: ";
+		cin >> N1;
+		cout << "\nVashe chuslo " << N1 << ((N1 < 0) ? ": videmne\n" : (N1 > 0) ? ": dodatne\n" : ": dorivnye nyly\n");
+		break;
+	case 4:
+		cout << "\nVvedite dva chusla: ";
+		cin >> N1 >> N2;
+		((N1 == N2) ? (cout << "Vashi chusla:  " << N1 << " ta " << N2 << " rivni\n")
+			: (N1 < N2) ? (cout << "Chusla za zrostannam:  " << N1 << ", " << N2 << endl)
+			: (cout << "Chusla za zrostannam:  " << N2 << ", " << N1 << endl));
+		break;
+	}
 }
